cb9bb4452ca267db9348ee68efe6b4e3a9fcfe63
Lusani-No1 <st10474461@rcconnect.edu.za>
2026-09-04 16:39:23 +0200

    User is there

--- RaceDay.sql
+++ RaceDay.sql
@@ -7,3 +7,17 @@ BEGIN
 END
 GO
 
+USE RaceDayDB;
+GO
+
+/* Drop tables if they already exist (in FK-safe order), so the script
+can be re-run cleanly on the same instance during testing. */
+
+IF OBJECT_ID('dbo.Results', 'U') IS NOT NULL DROP TABLE dbo.Results;
+IF OBJECT_ID('dbo.Enrolments', 'U') IS NOT NULL DROP TABLE dbo.Enrolments;
+IF OBJECT_ID('dbo.Categories', 'U') IS NOT NULL DROP TABLE dbo.Categories;
+IF OBJECT_ID('dbo.Events', 'U') IS NOT NULL DROP TABLE dbo.Events;
+IF OBJECT_ID('dbo.UserProfiles', 'U') IS NOT NULL DROP TABLE dbo.UserProfiles;
+IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL DROP TABLE dbo.Users;
+GO
+
